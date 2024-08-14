5512fddc424644c900901a53eaccd6e14bfd1df7
VanceGoblin <zvkubajak@gmail.com>
2024-08-14 16:25:56 -0500

    form.js and index.html Working Properly

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,16 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
 
+const mainElement = document.getElementById('main');
+const backButton = document.getElementById('back');
+
 // TODO: Create a function that builds an element and appends it to the DOM
+
+function buildElement(tag, text, className) {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  element.className = className;
+  mainElement.appendChild(element);
+}
 
 // TODO: Create a function that handles the case where there are no blog posts to display
 
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,5 +1,28 @@
 // TODO: Create a variable that selects the form element
 
+const submitForm = document.getElementById('submit');
+
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
 
+function getFormData () {
+  const formData = {
+    username: document.getElementById('username').value,
+    title: document.getElementById('title').value,
+    content: document.getElementById('content').value
+  };
+
+  console.log(formData);
+  
+}
+
+function formSubmission () {
+  getFormData();
+  redirectPage('blog.html');
+}
+
 // TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
+
+submitForm.addEventListener('click', function (event) {
+  event.preventDefault();
+  formSubmission();
+});
