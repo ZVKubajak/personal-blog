7c51f7460ba338d3d250f8d81c4f0777559e6c64
VanceGoblin <zvkubajak@gmail.com>
2024-08-15 10:06:27 -0500

    Added Working readLocalStorage Function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,24 +1,24 @@
-// * Array for blog data.
-
-let blogData = JSON.parse(localStorage.getItem('blogData'));
-
-if (!blogData) {
-  blogData = [];
-}
-
 // TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
 
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 
 function readLocalStorage () {
+  let blogData = JSON.parse(localStorage.getItem('blogData'));
 
+  if (!blogData) {
+    blogData = [];
+  }
+
+  return blogData;
 }
 
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
 
 function storeLocalStorage () {
+  readLocalStorage();
+
   const formData = JSON.parse(localStorage.getItem('formData'));
   blogData.push(formData);
 
@@ -26,10 +26,9 @@ function storeLocalStorage () {
   localStorage.setItem('blogData', updatedBlogData);
 
   console.log(JSON.parse(localStorage.getItem('blogData')));
+
+  return;
 }
-
-storeLocalStorage();
-
 
 // ! Use the following function whenever you need to redirect to a different page
 
